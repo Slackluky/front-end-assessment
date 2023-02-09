4bdb7fb181ba643e2d859b3c90180aa9bd0429d3
Slackluky <unluckly17@gmail.com>
2023-02-10 00:23:25 +0700

    add linter for code standard

---
+++ .eslintrc.cjs
@@ -0,0 +1,27 @@
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true,
+        node: true
+    },
+    extends: ["eslint:recommended", "plugin:vue/vue3-recommended", "prettier"],
+    parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module"
+    },
+    plugins: ["vue", "prettier"],
+    rules: {
+        "prettier/prettier": [
+            "error",
+            {
+                endOfLine: "auto",
+                semi: true,
+                tabWidth: 4,
+                printWidth: 100,
+                singleQuote: false,
+                trailingComma: "none",
+                jsxBracketSameLine: true
+            }
+        ]
+    }
+};
--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,30 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
+console.log("dawdaw");
 </script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo bg-blue-600" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+    <div>
+        <a href="https://vitejs.dev" target="_blank">
+            <img src="/vite.svg" class="logo vue" alt="Vite logo" />
+        </a>
+        <a href="https://vuejs.org/" target="_blank">
+            <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+        </a>
+    </div>
+    <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
 .logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+    height: 6em;
+    padding: 1.5em;
+    will-change: filter;
+    transition: filter 300ms;
 }
 .logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+    filter: drop-shadow(0 0 2em #646cffaa);
 }
 .logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+    filter: drop-shadow(0 0 2em #42b883aa);
 }
 </style>
